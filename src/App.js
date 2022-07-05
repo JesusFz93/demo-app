0933eb04aeba656288f1b89b8d6eb260ab1032f6
Jesus <jesus.fdz93@gmail.com>
2022-07-04 21:34:47 -0500

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import AppRouter from "./routers/AppRouter";
 
+import "./styles/estilos.css";
+
 const App = () => {
   return <AppRouter />;
 };
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  NavLink,
+} from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
@@ -7,9 +13,29 @@ import AboutPage from "../pages/AboutPage";
 const AppRouter = () => {
   return (
     <Router>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activo" : undefined)}
+          >
+            Home Page
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "activo" : undefined)}
+          >
+            About Page
+          </NavLink>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
