212c1210ad174502c677a164a80bb1585e1c676a
Jesus <jesus.fdz93@gmail.com>
2022-07-04 21:03:16 -0500

    Routes

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
 
 const AppRouter = () => {
   return (
     <Router>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </Router>
   );
 };
