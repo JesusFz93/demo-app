807c7e59b77c738bab65dd71967c16d80d95f315
Jesus <jesus.fdz93@gmail.com>
2022-07-08 11:33:18 -0500

    posts

--- src/pages/EstadosPage.jsx
+++ src/pages/EstadosPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 
+const initPosts = [];
+
 const EstadosPage = () => {
   const [contador, setCounter] = useState(0);
+  const [posts, setPosts] = useState(initPosts);
 
   const aumentarContador = () => {
     setCounter(contador + 1);
@@ -9,6 +12,17 @@ const EstadosPage = () => {
 
   const decrementarContador = () => {
     setCounter(contador - 1);
+  };
+
+  const extraerPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  const limpiarPosts = () => {
+    setPosts(initPosts);
   };
 
   return (
@@ -21,6 +35,17 @@ const EstadosPage = () => {
           <h2>Contador: {contador}</h2>
           <button onClick={aumentarContador}>Aumentar contador</button>
           <button onClick={decrementarContador}>Decrementar contador</button>
+          <button onClick={extraerPosts}>Extraer posts</button>
+          <button onClick={limpiarPosts}>Limpiar posts</button>
+          <ul>
+            {posts &&
+              posts.map((item) => (
+                <li key={item.id}>
+                  <h2>{item.title}</h2>
+                  <p>{item.body}</p>
+                </li>
+              ))}
+          </ul>
         </article>
       </main>
     </>
