955b099e882f4d51690675c0a56be8554f210f8b
Jesus <jesus.fdz93@gmail.com>
2022-07-11 20:26:39 -0500

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,64 +3,149 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          Home Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/about"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          About Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          Contact Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/help"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          Help Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/blog"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          Blog Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/ucamper/35/jesus"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          Ucamper Page
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/alumnos"
-          className={({ isActive }) => (isActive ? "activo" : undefined)}
-        >
-          Alumnos Page
-        </NavLink>
-      </li>
-    </ul>
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Home Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                About Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Contact Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/help"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Help Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/blog"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Blog Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/ucamper/35/jesus"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Ucamper Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/alumnos"
+                aria-current="page"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Alumnos Page
+              </NavLink>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
