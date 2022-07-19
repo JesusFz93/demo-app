a6f42fc060569d363e3376c9fd8aa0a01c0eb650
Jesus <jesus.fdz93@gmail.com>
2022-07-18 21:55:18 -0500

    mongo db

--- src/pages/PeliculasPage.jsx
+++ src/pages/PeliculasPage.jsx
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase/firebase";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+
+const initialForm = {
+  nombre: "",
+  descripcion: "",
+  precio: "",
+};
 
 const PeliculasPage = () => {
+  const [form, setForm] = useState(initialForm);
+  const [movies, setMovies] = useState([]);
+
+  const crearPelicula = async () => {
+    const coleccion = collection(db, "peliculas");
+    await addDoc(coleccion, form);
+    await obtenerPeliculas();
+  };
+
+  const eliminarPelicula = async (id) => {
+    const coleccion = doc(db, "peliculas", id);
+    await deleteDoc(coleccion);
+
+    await obtenerPeliculas();
+  };
+
+  const actualizarPelicula = async (id) => {
+    const coleccion = doc(db, "peliculas", id);
+    await updateDoc(coleccion, form);
+    await obtenerPeliculas();
+  };
+
+  const obtenerPeliculas = async () => {
+    const resp = await getDocs(collection(db, "peliculas"));
+    const peliculas = resp.docs.map((pelicula) => ({
+      id: pelicula.id,
+      ...pelicula.data(),
+    }));
+
+    setMovies(peliculas);
+  };
+
   return (
     <>
       <header>
@@ -17,6 +64,10 @@ const PeliculasPage = () => {
                 placeholder="Nombre"
                 autoComplete="off"
                 className="form-control"
+                value={form.nombre}
+                onChange={(e) => {
+                  setForm({ ...form, nombre: e.target.value });
+                }}
               />
             </div>
             <div className="mb-3">
@@ -27,6 +78,10 @@ const PeliculasPage = () => {
                 placeholder="Comentarios"
                 autoComplete="off"
                 className="form-control"
+                value={form.descripcion}
+                onChange={(e) => {
+                  setForm({ ...form, descripcion: e.target.value });
+                }}
               />
             </div>
             <div className="mb-3">
@@ -37,6 +92,10 @@ const PeliculasPage = () => {
                 placeholder="Precio"
                 autoComplete="off"
                 className="form-control"
+                value={form.precio}
+                onChange={(e) => {
+                  setForm({ ...form, precio: e.target.value });
+                }}
               />
             </div>
           </form>
@@ -45,13 +104,67 @@ const PeliculasPage = () => {
       <section>
         <article>
           <div className="btn-group" role="group" aria-label="Basic example">
-            <button type="button" className="btn btn-info">
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={obtenerPeliculas}
+            >
               Obtener peliculas
             </button>
-            <button type="button" className="btn btn-success">
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={crearPelicula}
+            >
               Guardar pelicula
             </button>
           </div>
+        </article>
+      </section>
+      <section>
+        <article>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Actualizar</th>
+                <th scope="col">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((obj) => {
+                return (
+                  <tr key={obj.id}>
+                    <td>{obj.id}</td>
+                    <td>{obj.nombre}</td>
+                    <td>{obj.descripcion}</td>
+                    <td>{obj.precio}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={() => actualizarPelicula(obj.id)}
+                      >
+                        Actualizar
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => eliminarPelicula(obj.id)}
+                      >
+                        Eliminar
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </article>
       </section>
     </>
